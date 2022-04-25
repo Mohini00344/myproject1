c792037c933c192c85cc35300fed923cdeb55ead
Mohini00344 <mohinigupta00344@gmail.com>
2022-04-25 11:38:56 +0530

    Programs/

---
+++ Programs/src/main/java/com/example/demo/ProgramsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramsApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
